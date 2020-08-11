f8e8d5a4ecae3f4694d66b5b0ace18b91eba1ebb
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2020-08-11 12:18:30 +0200

    Create Trace_CSP.tcl

---
+++ Trace_CSP.tcl
@@ -0,0 +1,11 @@
+#########################################################################
+# title: Trace_CSP.tcl                                                  #
+# author: Dario Garrido                                                 #
+# date: 20200811                                                        #
+# description: iRule for trace CSP policy                               #
+#########################################################################
+
+when HTTP_RESPONSE_RELEASE {
+    HTTP::header insert "Content-Security-Policy-Report-Only" [HTTP::header value "Content-Security-Policy"]
+    HTTP::header remove "Content-Security-Policy"
+}
