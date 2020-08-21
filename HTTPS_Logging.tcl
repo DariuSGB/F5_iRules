d3db8eecb5e6b8b2f53cac766ebebdba5b6e6275
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2020-08-22 01:01:06 +0200

    Create HTTPS_Logging.tcl

---
+++ HTTPS_Logging.tcl
@@ -0,0 +1,116 @@
+#########################################################################
+# title: HTTPS_Logging.tcl                                              #
+# author: Dario Garrido                                                 #
+# date: 20200822                                                        #
+# description: iRule for logging HTTPS requests                         #
+#########################################################################
+
+## INITIAL VARIABLE DECLARATION
+
+when RULE_INIT priority 50 {
+    # Client IP to log
+	set static::clientIP "10.1.1.1"
+}
+
+## REQUEST PRE HTTP PROFILE
+
+when CLIENTSSL_HANDSHAKE priority 50 {
+	set logging 0
+	if { [IP::client_addr] eq $static::clientIP } {
+		SSL::collect
+		set LogString "$static::clientIP:[TCP::client_port]"
+		set logging 1
+	}
+}
+when CLIENTSSL_DATA priority 50 {
+	set offset 0
+	log local0. "============================================="
+	log local0. "(REQ_PRE) $LogString"
+	foreach aHeader [split [SSL::payload] "\r\n"] {
+		if { $aHeader ne "" } {
+			log local0. $aHeader
+			set offset 0
+		} else {
+			if { $offset eq 2 } { break } else { incr offset }
+		}
+	}
+	SSL::release
+	log local0. "============================================="
+}
+
+## REQUEST POST HTTP PROFILE
+
+when HTTP_REQUEST priority 50 {
+	if {$logging} {
+		set LogString "$LogString -> [HTTP::method] [HTTP::host][HTTP::uri]"
+		log local0. "============================================="
+		log local0. "(REQ) $LogString"
+		foreach aHeader [HTTP::header names] {
+			log local0. "$aHeader: [HTTP::header value $aHeader]"
+		}
+		log local0. "============================================="
+	}
+}
+
+## REQUEST POST HTTP_REQUEST EVENT
+
+when HTTP_REQUEST_RELEASE priority 50 {
+	if {$logging} {
+		log local0. "============================================="
+		log local0. "(REQ_POST) $LogString"
+		foreach aHeader [HTTP::header names] {
+			log local0. "$aHeader: [HTTP::header value $aHeader]"
+		}
+		log local0. "============================================="
+	}
+}
+
+## RESPONSE PRE HTTP PROFILE
+
+when SERVERSSL_HANDSHAKE priority 50 {
+	if {$logging} {
+		SSL::collect
+	}
+}
+when SERVERSSL_DATA priority 50 {
+    set offset 0
+    log local0. "============================================="
+    log local0. "(REP_PRE) $LogString"
+    foreach aHeader [split [SSL::payload] "\r\n"] {
+        if { $aHeader ne "" } {
+            log local0. $aHeader
+            set offset 0
+        } else {
+            if { $offset eq 2 } { break } else { incr offset }
+        }
+    }
+    SSL::release
+    log local0. "============================================="
+}
+
+## RESPONSE POST HTTP PROFILE
+
+when HTTP_RESPONSE priority 50 {
+	if {$logging} {
+		set LogString "$LogString - [HTTP::status]"
+		log local0. "============================================="
+		log local0. "(REP) $LogString"
+		foreach aHeader [HTTP::header names] {
+			log local0. "$aHeader: [HTTP::header value $aHeader]"
+		}
+		log local0. "============================================="
+	}
+}
+
+## RESPONSE POST HTTP_RESPONSE EVENT
+
+when HTTP_RESPONSE_RELEASE priority 50 {
+	if {$logging} {
+		log local0. "============================================="
+		log local0. "(REP_POST) $LogString"
+		foreach aHeader [HTTP::header names] {
+			log local0. "$aHeader: [HTTP::header value $aHeader]"
+		}
+		log local0. "============================================="
+	}
+}
