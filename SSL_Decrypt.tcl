b6a2daf1be15c2f331f508e2760ccca49134b609
Dario Garrido <55515143+DariuSGB@users.noreply.github.com>
2021-04-29 16:45:33 +0200

    Create SSL_Decrypt.tcl

---
+++ SSL_Decrypt.tcl
@@ -0,0 +1,25 @@
+#########################################################################
+# title: SSL_Decrypt.tcl                                                #
+# author: Dario Garrido                                                 #
+# date: 20200409                                                        #
+# description: iRule for decrypting SSL keys                            #
+# https://support.f5.com/csp/article/K12783074                          #
+#########################################################################
+
+when CLIENTSSL_HANDSHAKE {
+	if { [IP::addr [getfield [IP::client_addr] "%" 1] equals 10.1.1.1] } {
+        log local0. "\[CSSL\][virtual name] [IP::client_addr]:[TCP::client_port] :: CLIENT_RANDOM [SSL::clientrandom] [SSL::sessionsecret]"
+		log local0. "\[CSSL\][virtual name] [IP::client_addr]:[TCP::client_port] :: RSA Session-ID:[SSL::sessionid] Master-Key:[SSL::sessionsecret]"
+	}
+}
+
+when SERVERSSL_HANDSHAKE {
+	if { [IP::addr [getfield [IP::client_addr] "%" 1] equals 10.1.1.1] } {
+        log local0. "\[SSSL\][virtual name] [IP::client_addr]:[TCP::client_port] :: CLIENT_RANDOM [SSL::clientrandom] [SSL::sessionsecret]"
+		log local0. "\[SSSL\][virtual name] [IP::client_addr]:[TCP::client_port] :: RSA Session-ID:[SSL::sessionid] Master-Key:[SSL::sessionsecret]"
+	}
+}
+
+### Export SSL Keys to file:
+# grep -h -o 'CLIENT_RANDOM.*' /var/log/ltm > /shared/tmp/sessionsecrets.pms
+# sed -e 's/^.*\(RSA Session-ID\)/\1/;tx;d;:x' /var/log/ltm >> /shared/tmp/sessionsecrets.pms
